9b109fe95ae6cb98e53cfe41a9a31051b6525633
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-05-16 02:51:13 -0700

    changed function name of respective pages

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-export default function About() {
+export default function Events() {
   return (
     <Layout>
       <div>This is the events page</div>
--- src/pages/history.js
+++ src/pages/history.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-export default function About() {
+export default function History() {
   return (
     <Layout>
       <div>This is the history page</div>
