837058a62d36546539e7fd34450dc0df1d32e821
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-05-23 21:35:48 -0700

    playing around with gatsby static image component

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,4 +1,4 @@
-import { Box, Center, Image } from '@chakra-ui/react';
+import { Box, Center } from '@chakra-ui/react';
 import { StaticImage } from "gatsby-plugin-image"
 import React from 'react';
 // import homeImage from '../images/homeImage.jpg'
@@ -9,7 +9,8 @@ const HomeContent = () => {
       <Center>
         <StaticImage
           src="../images/homeImage.jpg"
-          alt="heroImage" />
+          alt="heroImage"
+          placeholder="blurred" />
       </Center>
     </Box>
   )
