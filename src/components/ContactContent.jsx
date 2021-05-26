c69c99d48d6a14bb92c6d5525d429f74818f649a
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-05-25 17:40:36 -0700

    moved form validation from events to contact us

--- src/components/ContactContent.jsx
+++ src/components/ContactContent.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const ContactContent = () => {
+  const validateName = (value) => {
+    let error;
+    if (!value) {
+      error = "Name is required"
+    }
+    return error;
+  }
+
   return (
     <Box mr="2" ml="2">
       This will be the Contact us page content
