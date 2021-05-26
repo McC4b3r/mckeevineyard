88755ee13deeb23bbb2a3fc8f5f897b9da66136b
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-05-25 17:39:06 -0700

    added Formik for form validation on contact us

--- src/components/EventsContent.jsx
+++ src/components/EventsContent.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const EventsContent = () => {
+  const validateName = (value) => {
+    let error;
+    if (!value) {
+      error = "Name is required"
+    }
+    return error;
+  }
   return (
     <Box mr="2" ml="2">
       This is the content of the Events page
