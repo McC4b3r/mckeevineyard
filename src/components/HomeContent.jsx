aeb78ccec5fb560e0bc491a9b57d6c6d79c9babb
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-06-04 22:52:58 -0700

    fixed hero image bug, trying to use chakra image and gatsby static image

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,19 +1,20 @@
 import { Box, Center, Image } from '@chakra-ui/react';
 // import { StaticImage } from "gatsby-plugin-image"
 import React from 'react';
-
-const url = "https://nystudio107-ems2qegf7x6qiqq.netdna-ssl.com/img/blog/_992x558_crop_center-center_82_line/craft-cms-custom-field-leveraging-platform-2.jpg.webp"
+import homeImage from '../images/homeImage.jpg';
 
 const HomeContent = () => {
   return (
     <Box >
       <Center>
         <Image
-          src={url}
+          src={homeImage}
           alt="heroImage"
+          pos="relative"
           minH="100vh"
-          // placeholder="blurred"
-          minW="100vw" />
+          objectFit="cover"
+        // placeholder="blurred"
+        />
       </Center>
     </Box>
   )
