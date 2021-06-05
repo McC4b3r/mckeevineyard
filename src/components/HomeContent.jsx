332b8c987cbca64dd9e8837c8299bc7113c32a0c
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-06-04 22:26:43 -0700

    trying to adjust hero image mobile first

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,15 +1,19 @@
-import { Box, Center } from '@chakra-ui/react';
-import { StaticImage } from "gatsby-plugin-image"
+import { Box, Center, Image } from '@chakra-ui/react';
+// import { StaticImage } from "gatsby-plugin-image"
 import React from 'react';
+
+const url = "https://nystudio107-ems2qegf7x6qiqq.netdna-ssl.com/img/blog/_992x558_crop_center-center_82_line/craft-cms-custom-field-leveraging-platform-2.jpg.webp"
 
 const HomeContent = () => {
   return (
     <Box >
       <Center>
-        <StaticImage
-          src={"../images/homeImage.jpg"}
+        <Image
+          src={url}
           alt="heroImage"
-          placeholder="blurred" />
+          minH="100vh"
+          // placeholder="blurred"
+          minW="100vw" />
       </Center>
     </Box>
   )
