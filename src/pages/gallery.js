160811a893fe7a510729b3897add5ca38e4b66ce
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-06-14 22:59:02 -0700

    prepping gallery content organization

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout";
-import GalleryContent from "../components/GalleryContent";
+import GalleryContent from "../components/Gallery/GalleryContent";
 
 export default function Gallery() {
   return (
