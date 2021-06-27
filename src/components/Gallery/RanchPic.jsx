9308d021358b118932f73d3eb1b60e22fdf8bbde
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-06-26 22:10:47 -0700

    reworking ranchpic component

--- src/components/Gallery/RanchPic.jsx
+++ src/components/Gallery/RanchPic.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Box } from '@chakra-ui/react';
+import { Box, Image } from '@chakra-ui/react';
 // import { GatsbyImage } from "gatsby-plugin-image"
 
 const RanchPic = ({ url }) => (
   <Box boxSize="sm">
-    FUCK
+    <div>{url}</div>
   </Box>
 )
 
