a556b06d38f60339076b8e08c4bc3624941b3100
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-09-12 15:12:49 -0700

    hoping to fix menulink issue

--- src/components/Navbar/MenuLInks.jsx
+++ src/components/Navbar/MenuLInks.jsx
@@ -7,12 +7,12 @@ import {
 import LinkStack from "./LinkStack";
 
 const MenuLinks = ({ isOpen }) => {
-  const [isDesktop] = useMediaQuery("(minWidth: 768px")
+  const [isDesktop] = useMediaQuery("(min-width: 768px)")
 
   return (
     <Box
       mt="2"
-      flexBasis={{ base: "100%", md: "auto", lg: "auto", xl: "auto" }}
+      flexBasis={{ base: "100%", md: "auto" }}
     >
       {isDesktop ?
         <LinkStack />
