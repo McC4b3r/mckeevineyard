c000bfd1a729ba7adb8abb8b48d858609abab08f
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-11-09 17:51:21 -0800

    added content to about and history components

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import { Box } from "@chakra-ui/react"
+import { Box, Text, Heading } from "@chakra-ui/react"
 
 const AboutContent = () => {
+
+  const paragraph1 = <Text fontSize="md">
+    The wines from the vineyard are generally estate designated as from the Lucky Well Vineyard.
+  </Text>
+
+  const paragraph2 = <Text fontSize="md">
+    Looking at the older, as well as the more recent pictures of the house, it may appear the historic well is the reason for the vineyard designation.  Indeed, when Ray Furlong purchased the ranch in 1938 the well outside the house was known as having the sweetest water in the area and it never went dry.  Then again, in 1938 there was no electricity or means to supply the necessities of modern living: Indoor plumbing.
+  </Text>
+
+  const paragraph3 = <Text fontSize="md">
+    One of the first priorities Ray had was finding a location for a more dependable well which could meet the needs.  The plan was to find a source close by and pump the water to a water tower up in the pasture and let the force of gravity flow it downhill to the house. One or Ray’s other sisters, Claire, decided to try and dowse for water which produced the two wells close enough to the house and water tower with enough water for what then was one house, and later supplied a caretakers house, a guest house and pool as well.  At the time all the wells were hand dug shallow wells approximately 20 feet deep.  They never went dry.
+  </Text>
+
+  const paragraph4 = <Text fontSize="md">
+    When Ulises Valdez Sr. who designed and planted the vineyard, was looking for a well, he unsuccessfully tried a couple of locations.  In the process he struck up a conversation with our neighbor Bill Ruxton, whose father George was one of Ray’s best friends and hunting partners and bought their adjacent ranch, neighboring Ray’s in 1940, shortly after Ray bought his.  Bill pointed out a couple of other locations Claire had dowsed.  Ulises dug there and found the water that now supplies the vineyard and everything else.  Thereafter, Ulises named it the “Lucky Well Vineyard”.  So far, through several droughts, it has not run dry.
+  </Text>
+
+  const heading = <Heading>Lucky Well Vineyard</Heading>
   return (
     <Box mr="2" ml="2" >
-      This is the About Page Content!
+      {heading}
+      <br />
+      {paragraph1}
+      <br />
+      {paragraph2}
+      <br />
+      {paragraph3}
+      <br />
+      {paragraph4}
+      <br />
     </Box>
   )
 }
--- src/components/HistoryContent.jsx
+++ src/components/HistoryContent.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 
 const HistoryContent = () => {
+
+  const paragraph1 = <Text fontsize="md" >
+    The property was purchased in 1938 by my uncle Ray Furlong.  He was the son of Tom and Rosanna Furlong, my Grandmother Irene being the youngest daughter of 12 children, all born in the town of Bodega, California.  Tom Furlong was a native of Bree, county Wexford, Ireland.  He was one of four Furlong brothers who immigrated from Ireland between 1866 and 1875. Rosanna was born at Bodega Bay in 1856 to Andrew Johnson, an immigrant of Norway and Catherine Farrell from County Monaghan, Ireland, both arriving in California at the time of the Gold Rush.  Tom and Rosanna were married at St Theresa’s church Bodega in 1874.
+  </Text>
+
+  const paragraph2 = <Text fontsize="md">
+    At the time he bought it, what is now the vineyard consisted of 12 acres of Gravenstein apples and the rest was sheep pasture.  He later planted the rest in apples.  The two trees kept for sentimental reasons produce the apples for our annual pie day.  The balance of the orchard in now the vineyard.
+  </Text>
+
+  const paragraph3 = <Text fontsize="md">
+    The Furlong descendants were very much a part of the early history of the Bodega area of Sonoma County, north up the coast, west to Santa Rosa and south to Tomales and Petaluma.  After Rosanna’s daughter Mae (Mary) and husband Tom died in close proximity of one another, 1906 and 1907 respectively, several of the older children had already moved to Oakland so she and the remaining children followed first to Oakland then to Piedmont.
+  </Text>
+
+  const paragraph4 = <Text fontsize="md">
+    Many of Tom’s brother Patrick’s descendants have remained in Sonoma County and have remained active in the agriculture community up to and including the present.
+  </Text>
+
+  const paragraph5 = <Text fontsize="md">
+    Tom and Rosanna youngest daughter Irene, born February 21, 1893, married William Terrance McSorley, from another pioneering gold rush era family that settled in Mokelumne Hill in 1852 (near San Andreas in 1852).  Their only daughter, my mother Barbara was born August 23, 1923.
+  </Text>
+
+  const paragraph6 = <Text fontsize="md">
+    'Barbara so loved her Uncle Ray and his beloved ranch that when he died in December 1960, he left it to her.  She ran the ranch until 1990 when I took it over.'
+  </Text>
+
   return (
     <Box mr="2" ml="2">
-      This is the History page! Content coming soon!
+      {paragraph1}
+      <br />
+      {paragraph2}
+      <br />
+      {paragraph3}
+      <br />
+      {paragraph4}
+      <br />
+      {paragraph5}
+      <br />
+      {paragraph6}
+      <br />
     </Box>
   )
 }
